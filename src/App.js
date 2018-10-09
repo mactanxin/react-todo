b35f1be8c2e3f3f293f5d35e727fb396e60eb9ea
Xin Tan <mactanxin@gmail.com>
2018-10-09 15:02:24 -0700

    ADDED: comments on app constructor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoHeader from './components/TodoHeader'
 
+/*
+Todo app structure
+
+TodoApp
+	- TodoHeader
+	- TodoList
+    - TodoListItem #1
+		- TodoListItem #2
+		  ...
+		- TodoListItem #N
+	- TodoForm
+*/
+
+
 class App extends Component {
   render() {
     return (
